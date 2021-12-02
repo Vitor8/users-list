ddd08ec3143282d50ba9331c4f515b4539554b03
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-12-02 09:43:30 -0300

    Adiciona Requisito 5

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,11 +3,18 @@ import { connect } from 'react-redux';
 import * as userActions from '../actions/index';
 
 function UserCard({ user, removeUser }) {
+  
+  function confirmDelete(userId) {
+    const confirm = window.confirm('Deseja apagar esta linha ?');
+    if (confirm) removeUser(userId);
+    return null;
+  }
+
   return (
     <tr>
         <td className="table-row">{ user.name }</td>
         <td className="table-row">{ user.age }</td>
-        <button onClick={ () => removeUser(user.id) }>D</button>
+        <button onClick={ () => confirmDelete(user.id) }>D</button>
     </tr>
   );
 }
