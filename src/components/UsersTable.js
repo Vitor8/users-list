86a66cf986a5b748c187975e8bce48139d8a2e20
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-12-02 08:57:48 -0300

    Adiciona Requisito 3

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import UserCard from './UserCard';
 
 function UsersTable({ usersArray }) {
+  const [usersArrayOrderByAgeDec, setOrderDec] = useState([]);
+
+  useEffect(() => {
+    orderByAgeDec();
+  },[usersArray]);
+
+  function orderByAgeDec() {
+    let usersArrayDec = usersArray;
+    for (let i = 0; i < usersArrayDec.length; i++) {
+      for (let j = 0; j < (usersArrayDec.length - i - 1); j++) {
+        if (parseInt(usersArrayDec[j].age,10) < parseInt(usersArrayDec[j + 1].age,10)) {
+          let help = usersArrayDec[j];
+          usersArrayDec[j] = usersArrayDec[j + 1];
+          usersArrayDec[j + 1] = help;
+        }
+      }
+    }
+    setOrderDec(usersArrayDec);
+  }
+
   return (
     <div>
       <table>
@@ -10,7 +30,7 @@ function UsersTable({ usersArray }) {
           <th className="table-title">Nome</th>
           <th className="table-title">Idade</th>
         </tr>
-        { usersArray.map((user) => <UserCard user={ user } />) }
+        { usersArrayOrderByAgeDec.map((user) => <UserCard key={user.id} user={user} />) }
       </table>
     </div>
   );
